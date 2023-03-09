f846adaf12d7e1891c54b46be4dd2eb1a305ed65
Verge Robotics <VergeRoboticsFTC@gmail.com>
2023-03-09 16:49:52 -0600

    Testing commit and push from different computer

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     static DCMotor motor = new DCMotor(4);
     static Chassis chassis = new Chassis(0, 1, 2, 3);
     public static void main(String[] args) throws InterruptedException {
-
+        motor.moveTo(400, 0.5);
+        System.out.println("run teehee");
+        Thread.sleep(500);
+        motor.move(0.5);
     }
 }
